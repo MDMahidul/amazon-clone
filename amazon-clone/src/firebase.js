aaa4725a36373b9c57567800e19a12869cdc95a6
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2020-11-23 21:59:08 +0600

    Adding navbar

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDCdYES3QFpKHZxYcjvZ8tNxuRS5PywGVI",
+    authDomain: "clone-c1fde.firebaseapp.com",
+    databaseURL: "https://clone-c1fde.firebaseio.com",
+    projectId: "clone-c1fde",
+    storageBucket: "clone-c1fde.appspot.com",
+    messagingSenderId: "819317931610",
+    appId: "1:819317931610:web:26213c5af5bddbb6926263",
+    measurementId: "G-E8BJJ2LLFV"
+};
