c639aba78f148a17b9efd81e85d45415af828387
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2020-12-30 22:55:19 +0600

    Add to Basket and Remove from Basket
    Add product to the basket usign context api and then easily remove product from the basket.

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from "./component/Header";
 import Home from "./component/Home";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Checkout from "./component/Checkout";
 
 function App() {
   return (
       //BEM
-    <div className="App">
-        {/* Header */}
-        <Header/>
-        <Home/>
-        {/* Home */}
-    </div>
+   <Router>
+       <div className="App">
+           {/* Header */}
+           <Header/>
+          <Switch>
+              <Route path="/checkout" >
+                  <Checkout/>
+              </Route>
+              <Route path="/" >
+                  {/* Home */}
+                  <Home/>
+              </Route>
+          </Switch>
+       </div>
+   </Router>
   );
 }
 
--- amazon-clone/src/component/Home.js
+++ amazon-clone/src/component/Home.js
@@ -10,22 +10,53 @@ function Home(props) {
 
                 <div className="home__row">
                     <Product
-                        title="The learn startup"
+                        id="12321341"
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
                         price={19.19}
-                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
                         rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
                     />
-                    <Product />
+                    <Product
+                        id="12321348"
+                        title="Kenwood Mix Stand for Baking, Syylish Kitchen Mixer With K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                        price={239.78}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61etD4-IrPL._AC_SL1200_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product
+                        id="12321342"
+                        title="Fitbit Charge 3 Fitness Activity Tracker"
+                        price={32}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71YGrhhoqUL._AC_UL1500_.jpg"
+                    />
+                    <Product
+                        id="12321343"
+                        title="Amazon Echo (3rd generation) | Smart Speaker with Alexa, Charcoal Fabric"
+                        price={98.02}
+                        rating={4}
+                        image="https://www.scan.co.uk/images/products/3196456-a.jpg"
+                    />
+                    <Product
+                        id="12321344"
+                        title="New Apple iPad Pro (12.9-inch, Wi-Fi, 256GB) - Space Gray (4th Generation)"
+                        price={239.78}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81WhK5%2Bgf2L._AC_SL1500_.jpg"
+                    />
                 </div>
 
                 <div className="home__row">
-                    <Product />
+                    <Product
+                        id="12321342"
+                        title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                        price={199.78}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/41Fx-wYHMOL._AC_SY400_.jpg"
+                    />
                 </div>
             </div>
         </div>
