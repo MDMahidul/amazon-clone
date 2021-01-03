e79f2c518365187120694a7ec92a52eefeb4659c
MD. Mahidul Islam <47059408+MDMahidul@users.noreply.github.com>
2021-01-03 19:33:47 +0600

    User Sign-in & Sign-up Authentication
    in this step user can sign-in and signup with email and password. User firebase to store those data and use firebase authentication system for this signup and signin system.

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDCdYES3QFpKHZxYcjvZ8tNxuRS5PywGVI",
     authDomain: "clone-c1fde.firebaseapp.com",
@@ -9,3 +10,10 @@ const firebaseConfig = {
     appId: "1:819317931610:web:26213c5af5bddbb6926263",
     measurementId: "G-E8BJJ2LLFV"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth};
